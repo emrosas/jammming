ea3ad2961b40d8e488ec269052f9cf5475d53c25
Erick Mireles <emireles.rosas@gmail.com>
2023-11-18 19:31:29 -0600

    Added header component and bg image

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import signIn from "../../assets/sign-in.svg";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-comp">
+        <a href="/" className="header-link">
+          <img src={logo} className="logo" alt="Jammming Logo" />
+          <h1>
+            Ja<span className="green-span">mmm</span>ing
+          </h1>
+        </a>
+        <p className="medium-text">
+          This app will give you the power to search and select songs to create
+          a playlist right into your Spotify account.
+        </p>
+      </div>
+      <div className="sign-in">
+        <p className="sign-in-text  medium-text">Sign In</p>
+        <img src={signIn} alt="Sign In Icon" className="sign-in-icon" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
